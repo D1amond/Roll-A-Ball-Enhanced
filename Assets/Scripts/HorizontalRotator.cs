23b23ba5566bd1c7fe03c7de398fe635c02745b1
Frédéric Bourdeau <frebourd@hotmail.com>
2014-09-19 15:31:26 -0400

    ajout plateforme qui tourne

---
+++ Assets/Scripts/HorizontalRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalRotator : MonoBehaviour {
+
+	public float rotationSpeed;
+	public int rotationAngle;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, rotationAngle, 0) * Time.deltaTime * rotationSpeed);
+	}
+}
